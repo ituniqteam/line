e8e28d4494e132654f3bc20661497326b189c5a8
Ulanbekovvv <82395538+Alibek1106@users.noreply.github.com>
2023-02-26 19:46:13 +0600

    Add files via upload

--- js/swiper.js
+++ js/swiper.js
@@ -11,22 +11,19 @@ new Swiper('.our-team-slider', {
 });
 
 new Swiper('.comments-slider', {
-    slidesPerView: 1,
     spaceBetween: 30,
+    slidesPerView: 1,
+
+    breakpoints: {
+        840: {
+            spaceBetween: 69,
+            slidesPerView: 2,
+        },
+    },
 
     pagination: {
         el: ".slider-pagination",
         clickable: true,
     },
-
-    breakpoints: {
-        840: {
-            spaceBetween: 69
-        },
-
-        640: {
-            slidesPerView: 2
-        }
-    }
 })
 
